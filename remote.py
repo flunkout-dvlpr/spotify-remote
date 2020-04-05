5375f98b18fada684968997963af4fb779bf5d38
flunkout-dvlpr <info@juliogonzalez.space>
2020-04-05 01:46:44 -0500

    Update  remote.py file add button for play pause

--- remote.py
+++ remote.py
@@ -3,11 +3,13 @@ from spotipy import util
 from gpiozero import Button
 from time import sleep
 
+import spotify_	playback as playback
 button = Button(2)
 
 while True:
     if button.is_pressed:
         print("Pressed")
+        playback.playback()
     else:
         print("Released")
     sleep(1)
