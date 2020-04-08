54918bf844b6bfd2a7d286aa28b353db5ac4d99e
flunkout-dvlpr <info@juliogonzalez.space>
2020-04-07 22:35:27 -0500

    Remove any ’ from state message

--- lcd.py
+++ lcd.py
@@ -9,11 +9,11 @@ def connect():
 	
 def displayState(lcd, state):
 	lcd.clear()
-	state = [item.replace("'", "") for item in state]
-	print(state)
+	state = [item.replace("’", "") for item in state]
 	lcd.set_cursor_position(1, 1)
 	lcd.write(state[0])
 	if len(state) > 1:
 		lcd.set_cursor_position(1, 2)
 		lcd.write(state[1])
 
+  
